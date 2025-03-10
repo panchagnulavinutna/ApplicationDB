8576ec6394f7a32158955e5c27c48a511f6a1879
Vinutna <vinutnapanchagnula@gmail.com>
2025-03-10 09:32:06 +0530

    Add project files.

---
+++ ApplicationDB/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using ApplicationDB.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApplicationDB.Data
+    {
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Student> Students { get; set; }
+    }
+
+}
---
+++ ApplicationDB/Model/Student.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ApplicationDB.Models
+{
+    public class Student
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
